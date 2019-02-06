2c4e2714d2fc343d3e320aa0334cc1698154a6a5
Tristan White <oliver.tristan@gmail.com>
2019-02-06 10:48:25 +0100

    Rename project and add Github link

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     <div class="container">
         <div class="row">
             <div class="tiny-12 center-tiny">
-                <h1>MRKDWN, <i>the</i> lightest WYSIWYG Editor</h1>
+                <h1>MUDS Editor, <i>the</i> lightest WYSIWYG Editor</h1>
                 <p class="desc">Beautiful Javascript web editor that's easy to integrate for developers and your users will simply fall in love with its clean design.</p>
                 <br><br>
                 <a href="#0" class="button primary">Download</a>
-                <a href="#0" class="button secondary">Visit GitHub</a>
+                <a href="https://github.com/triss90/muds.io" target="_blank" class="button secondary">Visit GitHub</a>
                 <br><br><br><br>
                 <div id="editor"></div>
             </div>
@@ -20,7 +20,7 @@
     <div class="container">
         <div class="row center-tiny">
             <div class="tiny-12">
-                <h2>How to use mrkdwn editor</h2>
+                <h2>How to use muds editor</h2>
             </div>
         </div>
         <div class="row center-tiny">
@@ -31,20 +31,20 @@
 <span class="c4">&lt;!-- Create the editor container --></span>
 <span class="c1">&lt;div</span> <span class="c2">id</span><span class="c6">=</span><span class="c3">"editor"></span><span class="c1">&lt;/div></span>
 
-<span class="c4">&lt;!-- Include the mrkdwn library --></span>
+<span class="c4">&lt;!-- Include the muds library --></span>
 <span class="c1">&lt;script</span> <span class="c2">src</span><span class="c6">=</span><span class="c3">"/assets/js/editor.js"></span><span class="c1">&lt;/script></span>
 
-<span class="c4">&lt;!-- Initialize mrkdwn editor --></span>
+<span class="c4">&lt;!-- Initialize muds editor --></span>
 <span class="c1">&lt;script</span>
-<span class="c5">var</span> <span class="c6">editor =</span> <span class="c5">new</span> <span class="c2">mrkdwn</span><span class="c6">({</span>
+<span class="c5">var</span> <span class="c6">editor =</span> <span class="c5">new</span> <span class="c2">muds</span><span class="c6">({</span>
     <span class="c6">selector:</span> <span class="c3">'editor'</span>
 <span class="c6">});</span>
 <span class="c1">&lt;/script></span></pre>
             </div>
             <!--
             <div class="tiny-12 small-5 medium-6 large-6">
-                <pre class="code"><span class="c4">/* Initialize mrkdwn editor */</span>
-<span class="c5">var</span> <span class="c6">editor =</span> <span class="c5">new</span> <span class="c2">mrkdwn</span><span class="c6">({</span>
+                <pre class="code"><span class="c4">/* Initialize muds editor */</span>
+<span class="c5">var</span> <span class="c6">editor =</span> <span class="c5">new</span> <span class="c2">muds</span><span class="c6">({</span>
     <span class="c6">selector:</span> <span class="c3">'editor'</span><span class="c6">,</span>
     <span class="c6">menuStyle:</span> <span class="c3">'full'</span><span class="c6">,</span>
     <span class="c6">theme:</span> <span class="c3">'light'</span><span class="c6">,</span>
@@ -61,7 +61,7 @@
     <div class="container">
         <div class="row center-tiny">
             <div class="tiny-12">
-                <h2>MRKDWN</h2>
+                <h2>MUDS Editor</h2>
                 <br><br>
                 <p class="desc">Your powerful, lightweight editor.</p>
                 <br><br>
@@ -79,11 +79,11 @@
 <?php include('_inc/footer.php'); ?>
 
 <script>
-    var editor = new mrkdwn({
+    var editor = new muds({
         selector: 'editor',
         menuStyle: 'full', // full, minimal
         theme: 'light', // dark, light
-        content: '<h1>Try the mrkdwn editor today</h1><p>No matter what you\'re building, mrkdwn has you covered. From content management to email marketing, here are our customers\' most common uses:</p><ul><li>Content Management Systems (e.g. WordPress, Umbraco)</li><li>Learning Management Systems (e.g. Blackboard)</li><li>Customer Relationship Management and marketing automation (e.g. Marketo)</li></ul><br><p><a href="https://tristanwhite.info" target="_blank">Our team</a> is hard at work, creating and adding new features to take the Javascript web WYSIWYG editing capabilities to the next level.</p>',
+        content: '<h1>Try the muds editor today</h1><p>No matter what you\'re building, muds has you covered. From content management to email marketing, here are our customers\' most common uses:</p><ul><li>Content Management Systems (e.g. WordPress, Umbraco)</li><li>Learning Management Systems (e.g. Blackboard)</li><li>Customer Relationship Management and marketing automation (e.g. Marketo)</li></ul><br><p><a href="https://tristanwhite.info" target="_blank">Our team</a> is hard at work, creating and adding new features to take the Javascript web WYSIWYG editing capabilities to the next level.</p>',
         height: '300px',
     });
 </script>
