e3622bf75681f90200d2f0b73a29f3a0e217e669
Tristan White <oliver.tristan@gmail.com>
2019-02-07 18:11:32 +0100

    Adding darkmode demo to frontpage

--- index.php
+++ index.php
@@ -17,6 +17,15 @@
                 <br><br><br><br><br><br>
                 <div id="editor"></div>
             </div>
+            <div class="tiny-12">
+                <br>
+                <p>
+                    <input class="tgl tgl-light" id="cb1" type="checkbox" />
+                    <label class="tgl-btn" for="cb1"></label>
+                    <strong style="position:relative; bottom:10px; left:5px;">Try our new darkmode</strong>
+                </p>
+
+            </div>
         </div>
     </div>
 </header>
@@ -71,4 +80,22 @@ var editor = new muds({
         content: '<h1>Try the MUDS editor today</h1><p>No matter what you\'re building, MUDS has you covered. From content management to email marketing, here are our customers\' most common uses:</p><ul><li>Content Management Systems</li><li>Learning Management Systems</li><li>Customer Relationship Management and marketing automation</li></ul><br><p><a href="https://tristanwhite.info" target="_blank">Our team</a> is hard at work, creating and adding new features to take the Javascript web WYSIWYG editing capabilities to the next level.</p>',
         height: '300px'
     });
+
+
+    function themeSwitcher() {
+        var editor = document.getElementById('editor');
+        if (editor.classList.contains('light') === true) {
+            editor.classList.remove('light');
+            editor.classList.add('dark');
+        } else if (editor.classList.contains('dark') === true) {
+            editor.classList.remove('dark');
+            editor.classList.add('light');
+        }
+    }
+    var toggle = document.getElementById('cb1');
+    toggle.addEventListener('change', function() {
+        themeSwitcher();
+    });
+
+
 </script>
