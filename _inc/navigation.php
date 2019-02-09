63f92346933ff99002dc2a6069ded86c50c9774f
Tristan White <oliver.tristan@gmail.com>
2019-02-09 10:26:14 +0100

    Update changelog

--- _inc/navigation.php
+++ _inc/navigation.php
@@ -11,6 +11,6 @@
     <div class="navigation-items">
         <a href="/#demo">Installation guide</a>
         <a href="/documentation/">Documentaion</a>
-        <a href="/todo/">Todo</a>
+        <a href="/changelog/">Changes</a>
     </div>
 </nav>
---
+++ changelog/index.php
@@ -0,0 +1,67 @@
+<?php include('../_inc/header.php'); ?>
+
+<header id="menu-bg">
+    <div class="container">
+        <?php include('../_inc/navigation.php'); ?>
+    </div>
+</header>
+
+<section id="todo">
+    <div class="container">
+        <div class="row center-tiny start-medium">
+            <div class="tiny-12">
+                <h1>MUDS Editor changelog</h1>
+
+                <br>
+                <hr>
+                <br>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="tiny-12 medium-6">
+                <h2>MUDS v1.1.1</h2>
+                <ul>
+                    <li>Fixing dropdown menu bug where menu would be skewed to the right</li>
+                    <li>Fixing HTML editing in darkmode. Inactive buttons are new clearly indicated in darkmode</li>
+                </ul>
+
+                <br><br>
+
+                <h2>MUDS v1.1.0</h2>
+                <ul>
+                    <li>Adding support for editing HTML directly in the editor</li>
+                </ul>
+
+                <br><br>
+
+                <h2>MUDS v1.0.0</h2>
+                <ul>
+                    <li>We Launched MUDS Editor! Woohooo!</li>
+                </ul>
+
+            </div>
+            <div class="tiny-12 medium-6">
+                <h2>Our roadmap</h2>
+                <p>We're currently working and a lot exciting changes, among them are:</p>
+                <ul>
+                    <li>Table support</li>
+                    <li>Redesigned documentation</li>
+                    <li>Emoji menu (You can already use emojis in the editor)</li>
+                    <li><strike>A redesign of HTML editing mode.</strike></li>
+                    <li>Custom themes or some version of injected custom CSS</li>
+                    <li>Better image handling capabilities</li>
+                    <li>Color control (both text and background color)</li>
+                    <li>Custom font type support</li>
+                    <li>Custom keybindings</li>
+                    <li>Embedded video support</li>
+                    <li>Custom keybindings</li>
+                    <li>Code and quoteblock support</li>
+                    <li>In-, and outdent support</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include('../_inc/footer.php'); ?>
