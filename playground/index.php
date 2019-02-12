bf0746b46218f6601c00752db4324e2291b3420d
Tristan White <oliver.tristan@gmail.com>
2019-02-12 18:04:42 +0100

    Update dropdown visibility in darkmode

--- playground/index.php
+++ playground/index.php
@@ -56,7 +56,7 @@ dropdown-item >
             selector: 'editor',
             height: '342px',
             content: '<h1>Try the MUDS editor today</h1><p>No matter what you\'re building, <a href="#0">MUDS has you covered</a>. From content management to email marketing, here are our customers\' most common uses:</p><ul><li>Content Management Systems</li><li>Learning Management Systems</li><li>Customer Relationship Management and marketing automation</li></ul><p>Our team is hard at work, creating and adding new features to take the Javascript web WYSIWYG editing capabilities to the next level.</p>',
-            theme: "light",
+            theme: "dark",
             menuStyle: 'full',
             // menuCustom: [
             //     'header',
