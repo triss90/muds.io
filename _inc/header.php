f03b40381ec63a0ab36a83af58ecc41be27b0697
Tristan White <oliver.tristan@gmail.com>
2019-02-14 13:33:38 +0100

    Fix overflow problem on Font dropdown

--- _inc/header.php
+++ _inc/header.php
@@ -20,7 +20,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><!-- https://fontawesome.com/v4.7.0/ -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/gridder-css@1.5.1/gridder.min.css">
     <link rel="stylesheet" href="/assets/css/app.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/muds-editor@1.1.3/muds.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/muds-editor@1.2.0/muds.min.css">
 </head>
 <body>
 
