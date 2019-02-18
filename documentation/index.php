10730f2b8544dd50c905d471aba24312381f8bfa
Tristan White <oliver.tristan@gmail.com>
2019-02-18 08:13:47 +0100

    Publish 1.3.0

--- documentation/index.php
+++ documentation/index.php
@@ -54,7 +54,7 @@ var editor3 = new muds({
                     </pre>
                     <pre data-lang="html">
 &#x3C;!-- Include the MUDS stylesheet --&#x3E;
-&#x3C;link rel=&#x22;stylesheet&#x22; href=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.2.1/muds.min.css&#x22;&#x3E;
+&#x3C;link rel=&#x22;stylesheet&#x22; href=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.3.0/muds.min.css&#x22;&#x3E;
 
 &#x3C;b&#x3E;Full Editor&#x3C;/b&#x3E;
 &#x3C;!-- Create the editor container --&#x3E;
@@ -73,7 +73,7 @@ var editor3 = new muds({
 &#x3C;div id=&#x22;editor3&#x22;&#x3E;&#x3C;/div&#x3E;
 
 &#x3C;!-- Include the MUDS library --&#x3E;
-&#x3C;script src=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.2.1/muds.min.js&#x22;&#x3E;&#x3C;/script&#x3E;
+&#x3C;script src=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.3.0/muds.min.js&#x22;&#x3E;&#x3C;/script&#x3E;
                     </pre>
                 </div>
             </div>
@@ -157,13 +157,13 @@ var editor = new muds({
                     </pre>
                     <pre data-lang="html">
 &#x3C;!-- Include the MUDS stylesheet --&#x3E;
-&#x3C;link rel=&#x22;stylesheet&#x22; href=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.2.1/muds.min.css&#x22;&#x3E;
+&#x3C;link rel=&#x22;stylesheet&#x22; href=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.3.0/muds.min.css&#x22;&#x3E;
 
 &#x3C;!-- Create the editor container --&#x3E;
 &#x3C;div id=&#x22;editor&#x22;&#x3E;&#x3C;/div&#x3E;
 
 &#x3C;!-- Include the MUDS library --&#x3E;
-&#x3C;script src=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.2.1/muds.min.js&#x22;&#x3E;&#x3C;/script&#x3E;
+&#x3C;script src=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.3.0/muds.min.js&#x22;&#x3E;&#x3C;/script&#x3E;
                     </pre>
                 </div>
             </div>
@@ -191,7 +191,7 @@ var editorDark = new muds({
                     </pre>
                     <pre data-lang="html">
 &#x3C;!-- Include the MUDS stylesheet --&#x3E;
-&#x3C;link rel=&#x22;stylesheet&#x22; href=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.2.1/muds.min.css&#x22;&#x3E;
+&#x3C;link rel=&#x22;stylesheet&#x22; href=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.3.0/muds.min.css&#x22;&#x3E;
 
 &#x3C;!-- Create the editor container --&#x3E;
 &#x3C;div id=&#x22;editor&#x22;&#x3E;&#x3C;/div&#x3E;
@@ -202,7 +202,7 @@ var editorDark = new muds({
 &#x3C;div id=&#x22;editor-dark&#x22;&#x3E;&#x3C;/div&#x3E;
 
 &#x3C;!-- Include the MUDS library --&#x3E;
-&#x3C;script src=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.2.1/muds.min.js&#x22;&#x3E;&#x3C;/script&#x3E;
+&#x3C;script src=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.3.0/muds.min.js&#x22;&#x3E;&#x3C;/script&#x3E;
                     </pre>
                 </div>
             </div>
@@ -224,13 +224,13 @@ var editor = new muds({
                     </pre>
                     <pre data-lang="html">
 &#x3C;!-- Include the MUDS stylesheet --&#x3E;
-&#x3C;link rel=&#x22;stylesheet&#x22; href=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.2.1/muds.min.css&#x22;&#x3E;
+&#x3C;link rel=&#x22;stylesheet&#x22; href=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.3.0/muds.min.css&#x22;&#x3E;
 
 &#x3C;!-- Create the editor container --&#x3E;
 &#x3C;div id=&#x22;editor&#x22;&#x3E;&#x3C;/div&#x3E;
 
 &#x3C;!-- Include the MUDS library --&#x3E;
-&#x3C;script src=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.2.1/muds.min.js&#x22;&#x3E;&#x3C;/script&#x3E;
+&#x3C;script src=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.3.0/muds.min.js&#x22;&#x3E;&#x3C;/script&#x3E;
                     </pre>
                 </div>
             </div>
@@ -252,44 +252,44 @@ var editor = new muds({
                     </pre>
                     <pre data-lang="html">
 &#x3C;!-- Include the MUDS stylesheet --&#x3E;
-&#x3C;link rel=&#x22;stylesheet&#x22; href=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.2.1/muds.min.css&#x22;&#x3E;
+&#x3C;link rel=&#x22;stylesheet&#x22; href=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.3.0/muds.min.css&#x22;&#x3E;
 
 &#x3C;!-- Create the editor container --&#x3E;
 &#x3C;div id=&#x22;editor&#x22;&#x3E;&#x3C;/div&#x3E;
 
 &#x3C;!-- Include the MUDS library --&#x3E;
-&#x3C;script src=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.2.1/muds.min.js&#x22;&#x3E;&#x3C;/script&#x3E;
+&#x3C;script src=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.3.0/muds.min.js&#x22;&#x3E;&#x3C;/script&#x3E;
                     </pre>
                 </div>
             </div>
 
             <!-- Tooltips -->
-<!--            <div class="tiny-12">-->
-<!--                <h2>Tooltips</h2>-->
-<!--                <p>Tooltips are enabled by default, but can be disabled like so: <span class="tag">tooltips: false</span>.</p>-->
-<!--                <br>-->
-<!--                <h3>Example:</h3>-->
-<!--                <div class="codepen" data-height="400" data-theme-id="36048" data-default-tab="js,result" data-user="triss90" data-slug-hash="87854d8d260cfeef702dee5310ba6188" data-editable="false" data-prefill='{"title":"MUDS - EDITOR","tags":[],"stylesheets":[],"scripts":[]}'>-->
-<!--                    <pre data-lang="scss" data-option-autoprefixer></pre>-->
-<!--                    <pre data-lang="js">-->
-<!--// Initialize muds editor-->
-<!--var editor = new muds({-->
-<!--    selector: 'editor',-->
-<!--    tooltips: false-->
-<!--});-->
-<!--                    </pre>-->
-<!--                    <pre data-lang="html">-->
-<!--&#x3C;!-- Include the MUDS stylesheet --&#x3E;-->
-<!--&#x3C;link rel=&#x22;stylesheet&#x22; href=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.2.1/muds.min.css&#x22;&#x3E;-->
-<!---->
-<!--&#x3C;!-- Create the editor container --&#x3E;-->
-<!--&#x3C;div id=&#x22;editor&#x22;&#x3E;&#x3C;/div&#x3E;-->
-<!---->
-<!--&#x3C;!-- Include the MUDS library --&#x3E;-->
-<!--&#x3C;script src=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.2.1/muds.min.js&#x22;&#x3E;&#x3C;/script&#x3E;-->
-<!--                    </pre>-->
-<!--                </div>-->
-<!--            </div>-->
+            <div class="tiny-12">
+                <h2>Tooltips</h2>
+                <p>Tooltips are enabled by default, but can be disabled like so: <span class="tag">tooltips: false</span>.</p>
+                <br>
+                <h3>Example:</h3>
+                <div class="codepen" data-height="400" data-theme-id="36048" data-default-tab="js,result" data-user="triss90" data-slug-hash="87854d8d260cfeef702dee5310ba6188" data-editable="false" data-prefill='{"title":"MUDS - EDITOR","tags":[],"stylesheets":[],"scripts":[]}'>
+                    <pre data-lang="scss" data-option-autoprefixer></pre>
+                    <pre data-lang="js">
+// Initialize muds editor
+var editor = new muds({
+    selector: 'editor',
+    tooltips: false
+});
+                    </pre>
+                    <pre data-lang="html">
+&#x3C;!-- Include the MUDS stylesheet --&#x3E;
+&#x3C;link rel=&#x22;stylesheet&#x22; href=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.3.0/muds.min.css&#x22;&#x3E;
+
+&#x3C;!-- Create the editor container --&#x3E;
+&#x3C;div id=&#x22;editor&#x22;&#x3E;&#x3C;/div&#x3E;
+
+&#x3C;!-- Include the MUDS library --&#x3E;
+&#x3C;script src=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.3.0/muds.min.js&#x22;&#x3E;&#x3C;/script&#x3E;
+                    </pre>
+                </div>
+            </div>
 
             <!-- Resize Editor -->
             <div class="tiny-12">
@@ -308,75 +308,75 @@ var editor = new muds({
                     </pre>
                     <pre data-lang="html">
 &#x3C;!-- Include the MUDS stylesheet --&#x3E;
-&#x3C;link rel=&#x22;stylesheet&#x22; href=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.2.1/muds.min.css&#x22;&#x3E;
+&#x3C;link rel=&#x22;stylesheet&#x22; href=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.3.0/muds.min.css&#x22;&#x3E;
 
 &#x3C;!-- Create the editor container --&#x3E;
 &#x3C;div id=&#x22;editor&#x22;&#x3E;&#x3C;/div&#x3E;
 
 &#x3C;!-- Include the MUDS library --&#x3E;
-&#x3C;script src=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.2.1/muds.min.js&#x22;&#x3E;&#x3C;/script&#x3E;
+&#x3C;script src=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.3.0/muds.min.js&#x22;&#x3E;&#x3C;/script&#x3E;
                     </pre>
                 </div>
             </div>
 
             <!-- Keybindings -->
-<!--            <div class="tiny-12">-->
-<!--                <h2>Keybindings</h2>-->
-<!--                <p>Keybindings are enabled by default, but can be disabled like so: <span class="tag">keybindings: false</span>.</p>-->
-<!--                <br>-->
-<!--                <h3>Available keybindings:</h3>-->
-<!--                <ul>-->
-<!--                    <li><span class="tag">Tab</span>: inserts 4 spaces at the cursor position</li>-->
-<!--                    <li><span class="tag">Ctrl/Cmd + P</span>: Opens the print dialog</li>-->
-<!--                    <li><span class="tag">Ctrl/Cmd + B</span>: Makes highlighted text bold</li>-->
-<!--                    <li><span class="tag">Ctrl/Cmd + I</span>: Makes highlighted text italic</li>-->
-<!--                    <li><span class="tag">Ctrl/Cmd + A</span>: Highlight everything</li>-->
-<!--                    <li><span class="tag">Ctrl/Cmd + C</span>: Copy highlighted text</li>-->
-<!--                    <li><span class="tag">Ctrl/Cmd + X</span>: Cut highlighted text</li>-->
-<!--                    <li><span class="tag">Ctrl/Cmd + V</span>: Paste from clipboard</li>-->
-<!--                    <li><span class="tag">Ctrl/Cmd + Z</span>: Undo</li>-->
-<!--                    <li><span class="tag">Shift + Ctrl/Cmd + Z</span>: Redo</li>-->
-<!--                    <li><span class="tag">Ctrl/Cmd + Left Arrow</span>: Align Left</li>-->
-<!--                    <li><span class="tag">Ctrl/Cmd + Up Arrow/Down Arrow</span>: Align Center</li>-->
-<!--                    <li><span class="tag">Ctrl/Cmd + Right Arrow</span>: Align Right</li>-->
-<!--                    <li><span class="tag">Shift + Ctrl/Cmd + Right Arrow</span>: Indent</li>-->
-<!--                    <li><span class="tag">Shift + Ctrl/Cmd + Left Arrow</span>: Outdent</li>-->
-<!--                    <li><span class="tag">Shift + Ctrl/Cmd + C</span>: Open "Change Color" dialog</li>-->
-<!--                    <li><span class="tag">Ctrl/Cmd + L</span>: Open "Insert Link" dialog</li>-->
-<!--                    <li><span class="tag">Ctrl/Cmd + H</span>: Show HTML</li>-->
-<!--                    <li><span class="tag">Ctrl/Cmd + S</span>: Strikethrough</li>-->
-<!--                    <li><span class="tag">Ctrl/Cmd + L</span>: Open "Insert Link" dialog</li>-->
-<!--                    <li><span class="tag">Ctrl/Cmd + 0</span>: Body Text</li>-->
-<!--                    <li><span class="tag">Ctrl/Cmd + Q</span>: Blockquote</li>-->
-<!--                    <li><span class="tag">Ctrl/Cmd + 1</span>: Header 1</li>-->
-<!--                    <li><span class="tag">Ctrl/Cmd + 2</span>: Header 2</li>-->
-<!--                    <li><span class="tag">Ctrl/Cmd + 3</span>: Header 3</li>-->
-<!--                    <li><span class="tag">Ctrl/Cmd + .</span>: Unordered List</li>-->
-<!--                    <li><span class="tag">Ctrl/Cmd + ,</span>: Ordered List</li>-->
-<!--                </ul>-->
-<!--                <br>-->
-<!--                <h3>Example:</h3>-->
-<!--                <div class="codepen" data-height="400" data-theme-id="36048" data-default-tab="js,result" data-user="triss90" data-slug-hash="87854d8d260cfeef702dee5310ba6188" data-editable="false" data-prefill='{"title":"MUDS - EDITOR","tags":[],"stylesheets":[],"scripts":[]}'>-->
-<!--                    <pre data-lang="scss" data-option-autoprefixer></pre>-->
-<!--                    <pre data-lang="js">-->
-<!--// Initialize muds editor-->
-<!--var editor = new muds({-->
-<!--    selector: 'editor',-->
-<!--    keybindings: true-->
-<!--});-->
-<!--                    </pre>-->
-<!--                    <pre data-lang="html">-->
-<!--&#x3C;!-- Include the MUDS stylesheet --&#x3E;-->
-<!--&#x3C;link rel=&#x22;stylesheet&#x22; href=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.2.1/muds.min.css&#x22;&#x3E;-->
-<!---->
-<!--&#x3C;!-- Create the editor container --&#x3E;-->
-<!--&#x3C;div id=&#x22;editor&#x22;&#x3E;&#x3C;/div&#x3E;-->
-<!---->
-<!--&#x3C;!-- Include the MUDS library --&#x3E;-->
-<!--&#x3C;script src=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.2.1/muds.min.js&#x22;&#x3E;&#x3C;/script&#x3E;-->
-<!--                    </pre>-->
-<!--                </div>-->
-<!--            </div>-->
+            <div class="tiny-12">
+                <h2>Keybindings</h2>
+                <p>Keybindings are enabled by default, but can be disabled like so: <span class="tag">keybindings: false</span>.</p>
+                <br>
+                <h3>Available keybindings:</h3>
+                <ul>
+                    <li><span class="tag">Tab</span>: inserts 4 spaces at the cursor position</li>
+                    <li><span class="tag">Ctrl/Cmd + P</span>: Opens the print dialog</li>
+                    <li><span class="tag">Ctrl/Cmd + B</span>: Makes highlighted text bold</li>
+                    <li><span class="tag">Ctrl/Cmd + I</span>: Makes highlighted text italic</li>
+                    <li><span class="tag">Ctrl/Cmd + A</span>: Highlight everything</li>
+                    <li><span class="tag">Ctrl/Cmd + C</span>: Copy highlighted text</li>
+                    <li><span class="tag">Ctrl/Cmd + X</span>: Cut highlighted text</li>
+                    <li><span class="tag">Ctrl/Cmd + V</span>: Paste from clipboard</li>
+                    <li><span class="tag">Ctrl/Cmd + Z</span>: Undo</li>
+                    <li><span class="tag">Shift + Ctrl/Cmd + Z</span>: Redo</li>
+                    <li><span class="tag">Ctrl/Cmd + Left Arrow</span>: Align Left</li>
+                    <li><span class="tag">Ctrl/Cmd + Up Arrow/Down Arrow</span>: Align Center</li>
+                    <li><span class="tag">Ctrl/Cmd + Right Arrow</span>: Align Right</li>
+                    <li><span class="tag">Shift + Ctrl/Cmd + Right Arrow</span>: Indent</li>
+                    <li><span class="tag">Shift + Ctrl/Cmd + Left Arrow</span>: Outdent</li>
+                    <li><span class="tag">Shift + Ctrl/Cmd + C</span>: Open "Change Color" dialog</li>
+                    <li><span class="tag">Ctrl/Cmd + L</span>: Open "Insert Link" dialog</li>
+                    <li><span class="tag">Ctrl/Cmd + H</span>: Show HTML</li>
+                    <li><span class="tag">Ctrl/Cmd + S</span>: Strikethrough</li>
+                    <li><span class="tag">Ctrl/Cmd + L</span>: Open "Insert Link" dialog</li>
+                    <li><span class="tag">Ctrl/Cmd + 0</span>: Body Text</li>
+                    <li><span class="tag">Ctrl/Cmd + Q</span>: Blockquote</li>
+                    <li><span class="tag">Ctrl/Cmd + 1</span>: Header 1</li>
+                    <li><span class="tag">Ctrl/Cmd + 2</span>: Header 2</li>
+                    <li><span class="tag">Ctrl/Cmd + 3</span>: Header 3</li>
+                    <li><span class="tag">Ctrl/Cmd + .</span>: Unordered List</li>
+                    <li><span class="tag">Ctrl/Cmd + ,</span>: Ordered List</li>
+                </ul>
+                <br>
+                <h3>Example:</h3>
+                <div class="codepen" data-height="400" data-theme-id="36048" data-default-tab="js,result" data-user="triss90" data-slug-hash="87854d8d260cfeef702dee5310ba6188" data-editable="false" data-prefill='{"title":"MUDS - EDITOR","tags":[],"stylesheets":[],"scripts":[]}'>
+                    <pre data-lang="scss" data-option-autoprefixer></pre>
+                    <pre data-lang="js">
+// Initialize muds editor
+var editor = new muds({
+    selector: 'editor',
+    keybindings: true
+});
+                    </pre>
+                    <pre data-lang="html">
+&#x3C;!-- Include the MUDS stylesheet --&#x3E;
+&#x3C;link rel=&#x22;stylesheet&#x22; href=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.3.0/muds.min.css&#x22;&#x3E;
+
+&#x3C;!-- Create the editor container --&#x3E;
+&#x3C;div id=&#x22;editor&#x22;&#x3E;&#x3C;/div&#x3E;
+
+&#x3C;!-- Include the MUDS library --&#x3E;
+&#x3C;script src=&#x22;https://cdn.jsdelivr.net/npm/muds-editor@1.3.0/muds.min.js&#x22;&#x3E;&#x3C;/script&#x3E;
+                    </pre>
+                </div>
+            </div>
 
         </div>
     </div>
