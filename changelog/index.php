a705cc6f6156d07efeeecd299824f6114671b527
Tristan White <oliver.tristan@gmail.com>
2019-03-14 19:23:50 +0100

    Fix visual bug in markee theme menu

--- changelog/index.php
+++ changelog/index.php
@@ -21,12 +21,10 @@
         <div class="row">
             <div class="tiny-12 medium-7">
 
-                <h2>MUDS v1.5.6 <a href="/download/muds-editor-v1.5.6.zip" download><i class="fa fa-download" aria-hidden="true"></i> Download</a></h2>
+                <h2>MUDS v1.5.7 <a href="/download/muds-editor-v1.5.7.zip" download><i class="fa fa-download" aria-hidden="true"></i> Download</a></h2>
                 <ul>
-                    <li>Add theme: Markee - <a href="https://muds.io/documentation/themes/list/index.php#markee">Download</a></li>
-                    <li>Fix bug where placeholder could overflow editor</li>
-                    <li>Fix bug where content wouldn't be focused when placeholder was clicked</li>
-                    <li>Update menu buttons styles for all themes</li>
+                    <li>Fix visual bug in Markee theme menu</li>
+                    <li>Lighten color of character count</li>
                 </ul>
 
                 <br><br>
